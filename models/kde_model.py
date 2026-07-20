d9f457f42f2c90b32572f81885b94b20fdc80cf3
GabbeBytes19 <gabrielengstrom@hotmail.com>
2026-07-20 17:43:00 +0200

    Compared Gauss, KDE and Bayesian and wrote a markdown on the results

--- models/kde_model.py
+++ models/kde_model.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import stats
 
 def kde_estimate(df,lower_bound,upper_bound):
-    kde2 = stats.gaussian_kde(df,bw_method='silverman')
-    P2 = kde2.integrate_box_1d(lower_bound,upper_bound)
-    return P2
+    kde = stats.gaussian_kde(df,bw_method='silverman')
+    P = kde.integrate_box_1d(lower_bound,upper_bound)
+    return P
     
