bf0c7e60d28b1801d8a49158d9bbcdcf0c8f15a2
GabbeBytes19 <gabrielengstrom@hotmail.com>
2026-08-09 15:52:02 +0200

    Stake data we dont have aswell, need to fix this later aswell

--- backtest/engine.py
+++ backtest/engine.py
@@ -12,12 +12,13 @@ def engine(p_model,df_result):
 
     df_effective_edge = df_effective_edge.filter(pl.col("effective_edge_flag") == True)
 
-    df_side = df_effective_edge.with_columns(pl.when(pl.col("edge") > 0).then("Yes").otherwise("No").alias("side"))
+    df_side = df_effective_edge.with_columns(pl.when(pl.col("edge") > 0).then(pl.lit("Yes")).otherwise(pl.lit("No")).alias("side"))
 
 
     df_side = df_side.with_columns(pl.when(pl.col("side") == "Yes").then(df_side["market_prob"]).otherwise(1- df_side["market_prob"]).alias("price_paid"))
 
-    stake = df_side["stake"]
+    #stake = df_side["stake"]
+    stake = 0.05
 
     df_side = df_side.with_columns(pl.when(((pl.col("side") == "Yes") & (pl.col("win/loss flag") == 1))| ((pl.col("side") == "No") & (pl.col("win/loss flag") == 0))).then(stake * (1 / df_side["price_paid"] - 1) * (1 - settings.FEE_RATE)).otherwise(-stake).alias("profit"))
     df_final = df_side.with_columns(pl.col("profit").cumsum().alias("cumulative_profit"))
