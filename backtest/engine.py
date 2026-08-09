886fbc9138b7ab116b1b7e3e61595a99190bb88e
GabbeBytes19 <gabrielengstrom@hotmail.com>
2026-08-09 12:44:47 +0200

    Some extra touches to the engine

--- backtest/engine.py
+++ backtest/engine.py
@@ -10,16 +10,16 @@ def engine(p_model,df_result):
     #edge has date,marketprob,edge,stake, win/loss flag
     df_effective_edge = effective_edge(edge)
 
-    df_effective_edge = df_effective_edge.filter ((pl.col("effective_edge_flag") == True).alias("Only_True_Flag"))
+    df_effective_edge = df_effective_edge.filter(pl.col("effective_edge_flag") == True)
 
-    df_side = df_effective_edge.with_columns(pl.when(pl.col("Only_True_Flag") > 0).then("Yes").otherwise("No").alias("side"))
+    df_side = df_effective_edge.with_columns(pl.when(pl.col("edge") > 0).then("Yes").otherwise("No").alias("side"))
 
 
     df_side = df_side.with_columns(pl.when(pl.col("side") == "Yes").then(df_side["market_prob"]).otherwise(1- df_side["market_prob"]).alias("price_paid"))
 
-    stake = edge["stake"]
+    stake = df_side["stake"]
 
-    df_side = df_side.with_columns(pl.when(((pl.col("side") == "Yes") & (pl.col("win/loss flag") == 1)) & ((pl.col("side") == "No") & (pl.col("win/loss flag") == 0))).then(stake * (1 / df_side["price_paid"] - 1) * (1 - settings.FEE_RATE)).otherwise(-stake).alias("profit"))
+    df_side = df_side.with_columns(pl.when(((pl.col("side") == "Yes") & (pl.col("win/loss flag") == 1))| ((pl.col("side") == "No") & (pl.col("win/loss flag") == 0))).then(stake * (1 / df_side["price_paid"] - 1) * (1 - settings.FEE_RATE)).otherwise(-stake).alias("profit"))
     df_final = df_side.with_columns(pl.col("profit").cumsum().alias("cumulative_profit"))
 
     return df_final.sort("date")
